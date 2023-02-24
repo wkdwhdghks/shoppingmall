a9de8e97db527d3f31f5576c802466d59c534eec
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-02-24 16:25:39 +0900

    [Feat] 각각 좋아요 수 추가해주기

--- src/App.js
+++ src/App.js
@@ -7,21 +7,31 @@ function App() {
     "강남 우동맛집",
     "파이썬 독학",
   ]);
-  const [num, setNum] = useState(0);
+
+  const [number, setNumber] = useState([0, 0, 0]);
   const [modal, setModal] = useState(false);
 
   return (
     <div>
       {title.map((el, index) => (
         <div key={index}>
-          <p>
-            {el} <span onClick={() => setNum(num + 1)}>따봉</span> {num}
+          <p onClick={() => setModal(!modal)}>
+            {el}{" "}
+            <span
+              onClick={() => {
+                let arr = [...number];
+                arr[index] += 1;
+                setNumber(arr);
+              }}
+            >
+              따봉
+            </span>{" "}
+            {number[index]}
           </p>
           <p>2월 17일 발행</p>
         </div>
       ))}
       <button onClick={() => setTitle([...title].sort())}>버튼</button>
-      <button onClick={() => setModal(!modal)}>모달</button>
       {modal && <Modal />}
     </div>
   );
