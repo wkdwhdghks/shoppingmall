e4e56bd7e8e4a00d5774d3bc729ab599a1184e1c
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-02-27 16:22:39 +0900

    [Feat] react-Bootstrap 사용, navbar 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Navbar, Container, Nav } from "react-bootstrap";
+
 export default function App() {
   return (
     <div>
-      <p>h2</p>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
