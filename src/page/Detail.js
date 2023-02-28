6b7b045e6e1fdc2e17e7f3917e908300005a0e8d
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-02-28 17:56:05 +0900

    [Feat] 세일 타이머 구현

--- src/page/Detail.js
+++ src/page/Detail.js
@@ -1,11 +1,20 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function Detail({ shoes }) {
+  const [sale, setSale] = useState(true);
   const { id } = useParams();
   const findItme = shoes.find((item) => item.id === Number(id));
 
+  useEffect(() => {
+    setTimeout(() => {
+      setSale(false);
+    }, 2000);
+  }, []);
+
   return (
     <div className="container">
+      {sale && <div className="sale">2초 이내 구매 시 할인</div>}
       <div className="row">
         <div className="col-md-6">
           <img
